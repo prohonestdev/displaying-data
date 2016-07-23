ac9654a8b1258a39370add94afb96e25e1e52a3d
prohonestdev <sihndev@gmail.com>
2016-07-22 21:49:50 -0300

    step 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    	<h1>{{title}}</h1>
+    	<h2> My favorite hero is : {{myHero.name}}</h2>
+    	<p> heros </p>
+    	<ul>
+    		<li *ngFor = "let hero of heroes">
+    			{{hero.id}} - {{hero.name}}
+    		</li>
+    	</ul>
+    	<p *ngIf = "heroes.length > 3">
+    		There are many Heroes!
+    	</p>
+    	`
 })
-export class AppComponent { }
+export class AppComponent {
+	title = 'Tour of Heroes';
+	heroes = [
+		new Hero(1, 'Windstorm'),
+		new Hero(13, 'Bombasto'),
+		new Hero(15, 'Magneta'),
+		new Hero(20, 'Tornado')
+	];
+	myHero = this.heroes[0];
+}
