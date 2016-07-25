06cb6a4bb24beccb68525d9c41b2270dfc75ede8
prohonestdev <sihndev@gmail.com>
2016-07-24 23:25:40 -0300

    User Input with only click me.

--- app/hero.ts
+++ app/hero.ts
@@ -3,4 +3,3 @@ export class Hero {
 		public id: number,
 		public name: string){}
 	}
-}
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,7 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
+import { ClickMeComponent } from './click-me.component';
 
 bootstrap(AppComponent);
+bootstrap(ClickMeComponent)
