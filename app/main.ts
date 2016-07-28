8da9579d8910380be6b03f6c69b75aab5b08ddb9
prohonestdev <pronodedev@gmx.com>
2016-07-28 12:48:32 -0300

    look-back

--- app/main.ts
+++ app/main.ts
@@ -4,8 +4,10 @@ import { AppComponent } from './app.component';
 import { ClickMeComponent } from './click-me.component';
 import { KeyUpComponent_v1 } from './keyup.components';
 import { StrongKeyUpV1} from './strongkeyup.component';
+import { LoopbackComponent} from './loop-back.component';
 
 bootstrap(AppComponent);
 bootstrap(ClickMeComponent);
 bootstrap(KeyUpComponent_v1);
 bootstrap(StrongKeyUpV1);
+bootstrap(LoopbackComponent);
